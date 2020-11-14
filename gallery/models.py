f3d9ecde05b2695484f7656a3b278c6608b98b62
sawe-daisy <deesawe16@gmail.com>
2020-11-15 01:45:16 +0300

    app registration

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+impor datetime as dt
 # Create your models here.
+
+class Image(models.Model):
+    image=models.CharField()
+    name=models.CharField(max_length=10)
--- insta/settings.py
+++ insta/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'gallery',
+    'bootstrap4'
+
 ]
 
 MIDDLEWARE = [
