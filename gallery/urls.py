4ad2678d1f89d5a6f6afeb7bfd71c8e7f81928a4
sawe-daisy <deesawe16@gmail.com>
2020-11-15 01:28:35 +0300

    urls routing

---
+++ gallery/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf.urls.static import settings
+from . import views
+from django.urls import path, re_path
