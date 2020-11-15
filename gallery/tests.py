c0c5730b88e0780d9d6de3373ef8fdfeee1e4ecb
sawe-daisy <deesawe16@gmail.com>
2020-11-15 13:10:30 +0300

    testing model methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from .models import Image, Location, Category
 
 # Create your tests here.
+class Test_image(TestCase):
+
+    def setUp(self):
+        self.image=Image(name='name', image='image', 
+        description='description')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+    
+    def tearDown(self):
+        Image.objects.all().delete()
+
+class Location_Test(TestCase):
+
+    def setUP(self):
+        self.location=Location(location='location')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+    
+    def tearDown(self):
+        Location.objects.all.delete()
+
+class Category_Test(TestCase):
+    
+    def setUP(self):
+        self.location=Location(location='location')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+    
+    def tearDown(self):
+        Location.objects.all.delete()
