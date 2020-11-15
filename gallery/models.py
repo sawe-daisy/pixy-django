2064730ba3dff80b83667e00ce1492455b0131d3
sawe-daisy <deesawe16@gmail.com>
2020-11-15 03:37:17 +0300

    database

--- gallery/models.py
+++ gallery/models.py
@@ -4,3 +4,23 @@ import datetime as dt
 
 class Image(models.Model):
     name=models.CharField(max_length=10)
+    description = models.CharField(max_length=50)
+    location= models.ForeignKey(Location, on_delete=models.CASCADE, blank=True)
+    category = models.ForeignKey(Category, blank=True)
+    pub_date= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    location=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.location
+
+class Category(models.Model):
+    category=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.category
