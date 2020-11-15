35c08abc6f73e76bfeb1928213414a480c591b6f
sawe-daisy <deesawe16@gmail.com>
2020-11-15 03:47:13 +0300

    models registration

--- gallery/models.py
+++ gallery/models.py
@@ -2,17 +2,6 @@ from django.db import models
 import datetime as dt
 # Create your models here.
 
-class Image(models.Model):
-    name=models.CharField(max_length=10)
-    description = models.CharField(max_length=50)
-    location= models.ForeignKey(Location, on_delete=models.CASCADE, blank=True)
-    category = models.ForeignKey(Category, blank=True)
-    pub_date= models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Location(models.Model):
     location=models.CharField(max_length=20)
 
@@ -24,3 +13,18 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+class Image(models.Model):
+    name=models.CharField(max_length=10)
+    description = models.CharField(max_length=50)
+    location= models.ForeignKey(Location, on_delete=models.CASCADE, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True)
+    pub_date= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
