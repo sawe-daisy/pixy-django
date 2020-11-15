2431941cc0897b7bc6b72bd36d03bf8147dc8983
sawe-daisy <deesawe16@gmail.com>
2020-11-15 03:58:34 +0300

    updating urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,3 +3,7 @@ from django.conf.urls.static import static
 from django.conf.urls.static import settings
 from . import views
 from django.urls import path, re_path
+
+urlpatterns=[
+    path('search/', views.searchResults, name='searchResults'),
+]
